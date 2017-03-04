77799536231d123ca7b17a7a120fc26e61487b88
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-03-04 23:30:17 +0000

    Using method syntax

--- src/pretty_printer.rs
+++ src/pretty_printer.rs
@@ -1,28 +1,34 @@
 use ast::{Expr, Literal};
 
-pub fn pretty_print(expr: &Expr) -> String {
-    let mut pretty_printed = String::new();
-    match expr {
-        &Expr::Literal(ref l) => {
-            match l {
-                &Literal::StringLiteral(ref s) => pretty_printed.push_str(&s),
-                &Literal::NumberLiteral(n) => pretty_printed.push_str(&n.to_string()),
-            }
+impl Expr {
+    pub fn pretty_print(&self) -> String {
+        let mut pretty_printed = String::new();
+        match self {
+            &Expr::Literal(ref l) => pretty_printed.push_str(&l.pretty_print()),
+            _ => unimplemented!(),
         }
-        _ => unimplemented!(),
+        pretty_printed
     }
-    pretty_printed
+}
+
+impl Literal {
+    fn pretty_print(&self) -> String {
+        match self {
+            // TODO: avoid copying
+            &Literal::StringLiteral(ref s) => s.clone(),
+            &Literal::NumberLiteral(n) => n.to_string(),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use ast::*;
-    use pretty_printer::*;
 
     #[test]
     fn literal() {
         let expr = Expr::Literal(Literal::StringLiteral("abc".into()));
-        assert_eq!("abc", pretty_print(&expr));
+        assert_eq!("abc", &expr.pretty_print());
     }
 
     #[test]
@@ -38,6 +44,6 @@ mod tests {
             right: Expr::Grouping(Box::new(subexpr2)),
         };
         let expr = Expr::Binary(Box::new(binary_expr));
-        assert_eq!("(* (- 123) (group 45.67))", pretty_print(&expr));
+        assert_eq!("(* (- 123) (group 45.67))", &expr.pretty_print());
     }
 }
