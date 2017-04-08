08e864fef75ca140f999fb6bd83dade27e9577af
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-04-08 12:54:27 +0100

    Interpret for literals

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -20,10 +20,21 @@ pub trait Interpret {
 impl Interpret for Expr {
     fn interpret(&self) -> Result<Value, RuntimeError> {
         match self {
-            &Expr::Literal(ref l) => unimplemented!(),
+            &Expr::Literal(ref l) => l.interpret(),
             &Expr::Unary(ref u) => unimplemented!(),
             &Expr::Binary(ref b) => unimplemented!(),
             &Expr::Grouping(ref g) => unimplemented!(),
+        }
+    }
+}
+
+impl Interpret for Literal {
+    fn interpret(&self) -> Result<Value, RuntimeError> {
+        match self {
+            &Literal::NilLiteral => Ok(Value::Nil),
+            &Literal::BoolLiteral(b) => Ok(Value::Boolean(b)),
+            &Literal::StringLiteral(ref s) => Ok(Value::String(s.clone())),
+            &Literal::NumberLiteral(n) => Ok(Value::Number(n)),
         }
     }
 }
