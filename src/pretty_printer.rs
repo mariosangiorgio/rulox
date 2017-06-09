b423e8d45fdbd3a6aa0ddbc346e1a88538bead9c
Mario Sangiorgio <mariosangiorgio@gmail.com>
2017-06-09 23:07:09 +0100

    cargo fmt

--- src/pretty_printer.rs
+++ src/pretty_printer.rs
@@ -119,10 +119,10 @@ impl PrettyPrint for Statement {
                 e.pretty_print_into(pretty_printed);
                 pretty_printed.push_str(";");
             }
-            Statement::Expression(ref e) =>{
+            Statement::Expression(ref e) => {
                 e.pretty_print_into(pretty_printed);
                 pretty_printed.push_str(";");
-            },
+            }
             Statement::VariableDefinition(ref identifier) => {
                 pretty_printed.push_str("var ");
                 identifier.pretty_print_into(pretty_printed);
