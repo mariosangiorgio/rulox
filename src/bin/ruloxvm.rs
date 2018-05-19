524ae9da225ef5be24bebb2c7a8e0b0b0d6c37db
Mario Sangiorgio <mariosangiorgio@gmail.com>
2018-05-19 01:39:03 +0100

    Empty implementation for RuloxVm

--- src/bin/ruloxvm.rs
+++ src/bin/ruloxvm.rs
@@ -1,25 +1,12 @@
 extern crate rulox;
 
-use rulox::vm::bytecode::{disassemble, BinaryOp, Chunk, OpCode};
-use rulox::vm::vm::trace;
+use rulox::user_interface::Runner;
+use rulox::vm::RuloxVm;
+use std::env;
 
 fn main() {
-    println!("Usage: ruloxvm [script]");
-    let mut chunk = Chunk::new();
-    let mut offset = chunk.add_constant(1.2);
-    chunk.add_instruction(OpCode::Constant(offset), 123);
-    offset = chunk.add_constant(3.4);
-    chunk.add_instruction(OpCode::Constant(offset), 123);
-    chunk.add_instruction(OpCode::Binary(BinaryOp::Add), 123);
-    offset = chunk.add_constant(5.6);
-    chunk.add_instruction(OpCode::Constant(offset), 123);
-    chunk.add_instruction(OpCode::Binary(BinaryOp::Divide), 123);
-    chunk.add_instruction(OpCode::Negate, 123);
-    chunk.add_instruction(OpCode::Return, 123);
-
-    let stdout = std::io::stdout();
-    let handle = stdout.lock();
-    let mut writer = std::io::LineWriter::new(handle);
-    disassemble(&chunk, "test", &mut writer).unwrap();
-    trace(&chunk).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let mut runner = Runner::new(RuloxVm::new());
+    let exit_code = runner.run(&args);
+    std::process::exit(exit_code)
 }
--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,2 +1,18 @@
 pub mod bytecode;
 pub mod vm;
+
+use user_interface::{RuloxImplementation, RunResult as UiRunResult};
+
+pub struct RuloxVm {}
+
+impl RuloxVm {
+    pub fn new() -> RuloxVm {
+        RuloxVm {}
+    }
+}
+
+impl RuloxImplementation for RuloxVm {
+    fn run(&mut self, source: &str) -> UiRunResult {
+        UiRunResult::Error //TODO: implement
+    }
+}
