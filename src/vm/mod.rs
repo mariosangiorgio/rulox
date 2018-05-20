c6297e2315388008f14ea27b047da89755db7d99
Mario Sangiorgio <mariosangiorgio@gmail.com>
2018-05-20 23:59:50 +0100

    Exposed tokens (and relative errors) as an iterator
    This is much cleaner now that we have the impl Trait language feature

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -14,12 +14,12 @@ impl RuloxVm {
 
 impl RuloxImplementation for RuloxVm {
     fn run(&mut self, source: &str) -> UiRunResult {
-        let (tokens, errors) = scanner::scan(source);
-        for token in tokens {
-            println!("{:?}", token);
-        }
-        for error in errors {
-            println!("{:?}", error);
+        let mut errors = vec![];
+        for scan_result in scanner::scan_into_iterator(source) {
+            match scan_result {
+                Ok(token_with_context) => println!("{:?}", token_with_context),
+                Err(error) => errors.push(error), //TODO: wrap in generic error
+            }
         }
         UiRunResult::Error //TODO: implement
     }
